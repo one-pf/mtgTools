99f4dc6aacea819e9316478aab12711212733d8c
posmicanomaly <posmicanomaly@gmail.com>
2019-01-30 00:03:18 -0500

    improve setList demo

---
+++ src/app/mtg-card.ts
@@ -0,0 +1,23 @@
+export class MtgCard {
+  /* core */
+  arena_id;
+  id;
+  lang;
+  mtgo_id;
+  mtgo_foil_id;
+  multiverse_ids;
+  tcgplayer_id;
+  object;
+  oracle_id;
+  prints_search_uri;
+  rulings_uri;
+  scryfall_uri;
+  uri;
+
+  /* gameplay */
+  name;
+  cmc;
+
+  /* print */
+  image_uris;
+}
--- src/app/mtg/core/demo/set-list/set-list.component.ts
+++ src/app/mtg/core/demo/set-list/set-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ScryfallService } from 'src/app/scryfall.service';
 import { MtgSet } from 'src/app/mtg-set';
+import { MtgCard } from 'src/app/mtg-card';
 
 @Component({
   selector: 'app-set-list',
@@ -9,6 +10,11 @@ import { MtgSet } from 'src/app/mtg-set';
 })
 export class SetListComponent implements OnInit {
   sets: Array<MtgSet> = [];
+  selectedSet: MtgSet;
+  selectedCard: MtgCard;
+  rarityFilter = 'all';
+  isLoadingSelectedSet = false;
+  cardSet: Array<MtgCard> = [];
   // fields = Object.keys(new MtgSet());
   fields = [
     'block', 'block_code', 'card_count', 'code', 'digital',
@@ -17,7 +23,11 @@ export class SetListComponent implements OnInit {
     'set_type', 'tcgplayer_id', 'uri'
   ];
 
-  coreFields = ['name', 'card_count', 'released_at'];
+  coreFields = [
+    {key: 'name', text: 'Name'},
+    {key: 'card_count', text: 'Cards'},
+    {key: 'released_at', text: 'Release Date'}
+  ];
 
   constructor(private api: ScryfallService) { }
 
@@ -30,12 +40,56 @@ export class SetListComponent implements OnInit {
 
   getSets(setType) {
     const filtered = this.sets
-      .filter(set => set['set_type'] === 'core')
+      .filter(set => set['set_type'] === setType)
       .sort((a, b) => {
         if (a.released_at > b.released_at) { return 1; }
         if (a.released_at < b.released_at) { return -1; }
         return 0;
       });
     return filtered.reverse();
+  }
+
+  getFilteredCardSet() {
+    if (this.rarityFilter === 'all') {
+      return this.cardSet;
+    }
+    return this.cardSet.filter(card => {
+      return card['rarity'] === this.rarityFilter;
+    });
+  }
+
+  selectSet(set) {
+    this.selectedSet = set;
+    console.log(this.selectedSet);
+    this.cardSet = [];
+    this.isLoadingSelectedSet = true;
+    this.loadSelectedSet(set.search_uri);
+  }
+
+  selectCard(card) {
+    this.selectedCard = card;
+  }
+
+  private loadSelectedSet(uri) {
+    this.api.getSearchResult(uri).subscribe(response => {
+      console.log(response);
+      this.cardSet = this.cardSet.concat(<Array<MtgCard>> response['data']);
+      if (response['has_more']) {
+        console.log('has more');
+        this.loadSelectedSet(response['next_page']);
+      } else {
+        this.cardSet.forEach(card => {
+          if (!card['usd']) {
+            card['usd'] = '0.00';
+          }
+        });
+        this.cardSet.sort((a, b) => {
+          if (+a['usd'] > +b['usd']) { return 1; }
+          if (+a['usd'] < +b['usd']) { return -1; }
+          return 0;
+        }).reverse();
+        this.isLoadingSelectedSet = false;
+      }
+    });
   }
 }
